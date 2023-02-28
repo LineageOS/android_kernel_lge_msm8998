4c3745bf3c2317fc643f1674c2a16abd96598fed
Han Sol Jin <hansol@hansol.ca>
2023-02-27 19:36:44 -0800

    Revert "qcacld: defer hdd initialization and rely on userspace writing to /dev/wlan"
    This reverts commit 1004d4cb38e06eeecfdfd4a1edb1afc3d62a5f67.
    
    Change-Id: I9b7b891c88af2f2d8549598854492af44af38baf

--- drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
+++ drivers/staging/qcacld-3.0/core/hdd/src/wlan_hdd_main.c
@@ -145,7 +145,6 @@ static unsigned int dev_num = 1;
 static struct cdev wlan_hdd_state_cdev;
 static struct class *class;
 static dev_t device;
-static bool hdd_loaded = false;
 
 #define HDD_OPS_INACTIVITY_TIMEOUT (120000)
 #define MAX_OPS_NAME_STRING_SIZE 20
@@ -12351,7 +12350,6 @@ static int wlan_hdd_state_ctrl_param_open(struct inode *inode,
 	return 0;
 }
 
-static int __hdd_module_init(void);
 static ssize_t wlan_hdd_state_ctrl_param_write(struct file *filp,
 						const char __user *user_buf,
 						size_t count,
@@ -12380,13 +12378,6 @@ static ssize_t wlan_hdd_state_ctrl_param_write(struct file *filp,
 	if (strncmp(buf, wlan_on_str, strlen(wlan_on_str)) != 0) {
 		pr_err("Invalid value received from framework");
 		goto exit;
-	}
-
-	if (!hdd_loaded) {
-		if (__hdd_module_init()) {
-			pr_err("%s: Failed to init hdd module\n", __func__);
-			goto exit;
-		}
 	}
 
 	if (!cds_is_driver_loaded()) {
@@ -12507,7 +12498,12 @@ static int __hdd_module_init(void)
 		goto out;
 	}
 
-	hdd_loaded = true;
+	ret = wlan_hdd_state_ctrl_param_create();
+	if (ret) {
+		pr_err("wlan_hdd_state_create:%x\n", ret);
+		goto out;
+	}
+
 	pr_info("%s: driver loaded\n", WLAN_MODULE_NAME);
 
 	return 0;
@@ -12553,13 +12549,12 @@ static void __hdd_module_exit(void)
  */
 static int hdd_module_init(void)
 {
-	int ret;
+	if (__hdd_module_init()) {
+		pr_err("%s: Failed to register handler\n", __func__);
+		return -EINVAL;
+	}
 
-	ret = wlan_hdd_state_ctrl_param_create();
-	if (ret)
-		pr_err("wlan_hdd_state_create:%x\n", ret);
-
-	return ret;
+	return 0;
 }
 
 /**
